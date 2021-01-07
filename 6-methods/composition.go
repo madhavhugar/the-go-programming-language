a84a0e9125665620dc7b1bd3342d7b7ec3200ec1
Madhav Hugar <madhav.hugar@gmail.com>
2021-01-07 15:36:40 +0100

    add more composition scenarios

--- 6-methods/composition.go
+++ 6-methods/composition.go
@@ -1,15 +1,49 @@
-// method promotion with one embedded field
 package geometry
 
 import (
 	"image/color"
 )
 
+// ColoredPoint objects can access the methods of Point
+// method promotion with one embedded field
 type ColoredPoint struct {
 	Point
 	Color color.RGBA
 }
 
-// struct with multiple anonymous fields
+// Shape represents 2D or 3D geometric shape
+type Shape interface {
+	area() float64
+}
 
-// unnamed structs types methods
+// Square represents a square shape
+type Square struct {
+	length float64
+}
+
+func (s Square) area() float64 {
+	return s.length * s.length
+}
+
+// Rectangle represents a rectangle shape
+type Rectangle struct {
+	length  float64
+	breadth float64
+}
+
+func (r Rectangle) area() float64 {
+	return r.length * r.breadth
+}
+
+// ColoredShapes objects can access methods of Square and Rectangle
+// struct with multiple anonymous fields
+type ColoredShapes struct {
+	Square
+	Rectangle
+	Color color.RGBA
+}
+
+// ShapeArea computes the area of a given shape object
+func ShapeArea(s Shape) float64 {
+	return s.area()
+}
--- 6-methods/composition_test.go
+++ 6-methods/composition_test.go
@@ -15,3 +15,51 @@ func TestColoredPoint_PromotedMethod_Distance(t *testing.T) {
 		t.Errorf("%f != %f", got, expected)
 	}
 }
+
+func TestColoredShape_PromotedMethods(t *testing.T) {
+	cs := ColoredShapes{Square{10}, Rectangle{10, 20}, color.RGBA{1, 1, 1, 1}}
+
+	// cs.area()
+	// will not work since go will complain about ambigious selection of promoted methods
+	// both Rectange and Square have area method. So we have to explicitly specify which methods
+	// needs to be called
+	sqArea := cs.Square.area()
+	reArea := cs.Rectangle.area()
+
+	if sqArea != 100 {
+		t.Errorf("Incorrect square area")
+	}
+
+	if reArea != 200 {
+		t.Errorf("Incorrect rectangle area")
+	}
+}
+
+func TestShapeArea(t *testing.T) {
+	s := Square{10}
+	r := Rectangle{10, 20}
+
+	sqArea := ShapeArea(s)
+	reArea := ShapeArea(r)
+
+	if sqArea != 100 {
+		t.Errorf("Incorrect square area")
+	}
+
+	if reArea != 200 {
+		t.Errorf("Incorrect rectangle area")
+	}
+}
+
+func TestAnonymousStruct(t *testing.T) {
+	// only via embedding an anonymous struct can have methods
+	twoDimShape := struct {
+		Rectangle
+	}{
+		Rectangle: Rectangle{10, 20},
+	}
+
+	if twoDimShape.area() != 200 {
+		t.Errorf("Incorrect rectangle area")
+	}
+}
