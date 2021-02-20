0f07a7b6db61fa5bcc2115270dcc1fa424b52d9f
Madhav Hugar <madhav.hugar@gmail.com>
2021-02-20 22:14:25 +0100

    thumbnail converter: an embaressingly parallel problem :pipeline:

--- 8-channels/thumbnail.go
+++ 8-channels/thumbnail.go
@@ -9,3 +9,34 @@ func thumbnailConverter(filepath string) (thumbnailPath string) {
 	time.Sleep(time.Second * 1)
 	return fmt.Sprintf("%s_thumbnail", filepath)
 }
+
+func howNotToWriteParallelGoRoutines(files []string) {
+	for _, file := range files {
+		// Will exit quickly
+		// does not wait for the goroutines to finish
+		go thumbnailConverter(file)
+	}
+}
+
+func thumbnailConverter1(filepath string) (thumbnailPath string, err error) {
+	time.Sleep(time.Second * 1)
+	return fmt.Sprintf("%s_thumbnail", filepath), nil
+}
+
+// Parallel go routines without error handling
+func parallelGoRoutinesWithUnbufferedChannel(files []string) []string {
+	thumbnails := make(chan string)
+	var pathnames []string
+
+	for _, file := range files {
+		go func(f string) {
+			thumbnail, _ := thumbnailConverter1(f)
+			thumbnails <- thumbnail
+		}(file)
+	}
+
+	for range files {
+		pathnames = append(pathnames, <-thumbnails)
+	}
+	return pathnames
+}
--- 8-channels/thumbnail_test.go
+++ 8-channels/thumbnail_test.go
@@ -1,6 +1,10 @@
 package channels
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestThumbnailConverter(t *testing.T) {
 	got := thumbnailConverter("kitty")
@@ -9,4 +13,12 @@ func TestThumbnailConverter(t *testing.T) {
 	if got != wanted {
 		t.Errorf("got != wanted; %s != %s", got, wanted)
 	}
+}
+
+func TestParallelGoRoutinesWithUnbufferedChannel(t *testing.T) {
+	files := []string{"kitty", "foo", "cookie", "bar"}
+	got := parallelGoRoutinesWithUnbufferedChannel(files)
+	expected := []string{"kitty_thumbnail", "foo_thumbnail", "cookie_thumbnail", "bar_thumbnail"}
+
+	assert.ElementsMatch(t, expected, got)
 }
