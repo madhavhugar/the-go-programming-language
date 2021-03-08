c6027a13786b355ac5595a103959c2b324c99009
Madhav Hugar <madhav.hugar@gmail.com>
2021-03-08 22:23:10 +0100

    using new ticker to avoid go routine leak

---
+++ 8-channels/slv/satelliteLaunchTimerWithAbortAndNewTicker.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+var aborting = make(chan struct{})
+
+func pressAbort() {
+	os.Stdin.Read(make([]byte, 1))
+	aborting <- struct{}{}
+}
+
+func main() {
+	ticker := time.NewTicker(time.Second)
+	go pressAbort()
+	for i := 10; i >= 0; i-- {
+		select {
+		case <-ticker.C:
+			fmt.Printf("%d...\n", i)
+		case <-aborting:
+			fmt.Println("Aborting launch")
+			os.Exit(1)
+		}
+	}
+	ticker.Stop()
+}
